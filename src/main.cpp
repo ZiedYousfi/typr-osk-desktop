816eaec6e274c4f3f851ac2d6ed8b4cf455c4b47
ZiedYousfi <yousfi_zied@yahoo.com>
2025-12-16 20:14:47 +0100

    Update clangd config and tidy main.cpp
    - Use directory for CompilationDatabase instead of a file path
    - Add space in `char **argv` signature and ensure newline at EOF

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <QApplication>
 #include <QLabel>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   QApplication app(argc, argv);
 
   QLabel label("Hello Qt + Meson + Conan!");
