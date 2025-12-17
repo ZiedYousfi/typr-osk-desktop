0e5ecf09e65907fd324c91d4dd44321155f30e32
ZiedYousfi <yousfi_zied@yahoo.com>
2025-12-17 10:38:51 +0100

    Extend Key enum and add Unicode character input
    - Expand Key to uint16_t with explicit values and many new keys
    - Add optional character field to KeyStroke for Unicode input
    - Provide overloads for keyDown/keyUp/tap that accept KeyStroke
    - Implement typeCharacter and character handling in the macOS backend

--- src/input/input.hpp
+++ src/input/input.hpp
@@ -1,111 +1,206 @@
 // input.hpp
 #pragma once
 #include <cstdint>
+#include <optional>
 #include <string>
 
 namespace input {
 
-enum class Key : uint8_t {
+enum class Key : uint16_t {
   Unknown = 0,
-  A,
-  B,
-  C,
-  D,
-  E,
-  F,
-  G,
-  H,
-  I,
-  J,
-  K,
-  L,
-  M,
-  N,
-  O,
-  P,
-  Q,
-  R,
-  S,
-  T,
-  U,
-  V,
-  W,
-  X,
-  Y,
-  Z,
-  Num0,
-  Num1,
-  Num2,
-  Num3,
-  Num4,
-  Num5,
-  Num6,
-  Num7,
-  Num8,
-  Num9,
-  Enter,
-  Escape,
-  Backspace,
-  Tab,
-  Space,
-  Left,
-  Right,
-  Up,
-  Down,
-  F1,
-  F2,
-  F3,
-  F4,
-  F5,
-  F6,
-  F7,
-  F8,
-  F9,
-  F10,
-  F11,
-  F12,
+  A = 1,
+  B = 2,
+  C = 3,
+  D = 4,
+  E = 5,
+  F = 6,
+  G = 7,
+  H = 8,
+  I = 9,
+  J = 10,
+  K = 11,
+  L = 12,
+  M = 13,
+  N = 14,
+  O = 15,
+  P = 16,
+  Q = 17,
+  R = 18,
+  S = 19,
+  T = 20,
+  U = 21,
+  V = 22,
+  W = 23,
+  X = 24,
+  Y = 25,
+  Z = 26,
+  Num0 = 33,
+  Num1 = 34,
+  Num2 = 35,
+  Num3 = 36,
+  Num4 = 37,
+  Num5 = 38,
+  Num6 = 39,
+  Num7 = 40,
+  Num8 = 41,
+  Num9 = 42,
+  F1 = 43,
+  F2 = 44,
+  F3 = 45,
+  F4 = 46,
+  F5 = 47,
+  F6 = 48,
+  F7 = 49,
+  F8 = 50,
+  F9 = 51,
+  F10 = 52,
+  F11 = 53,
+  F12 = 54,
+  F13 = 55,
+  F14 = 56,
+  F15 = 57,
+  F16 = 58,
+  F17 = 59,
+  F18 = 60,
+  F19 = 61,
+  F20 = 62,
+  Enter = 63,
+  Escape = 64,
+  Backspace = 65,
+  Tab = 66,
+  Space = 67,
+  Left = 68,
+  Right = 69,
+  Up = 70,
+  Down = 71,
+  Home = 72,
+  End = 73,
+  PageUp = 74,
+  PageDown = 75,
+  Delete = 76,
+  Insert = 77,
+  PrintScreen = 78,
+  ScrollLock = 79,
+  Pause = 80,
+  NumpadDivide = 83,
+  NumpadMultiply = 84,
+  NumpadMinus = 85,
+  NumpadPlus = 86,
+  NumpadEnter = 87,
+  NumpadDecimal = 88,
+  Numpad0 = 89,
+  Numpad1 = 90,
+  Numpad2 = 91,
+  Numpad3 = 92,
+  Numpad4 = 93,
+  Numpad5 = 94,
+  Numpad6 = 95,
+  Numpad7 = 96,
+  Numpad8 = 97,
+  Numpad9 = 98,
+  ShiftLeft = 99,
+  ShiftRight = 100,
+  CtrlLeft = 101,
+  CtrlRight = 102,
+  AltLeft = 103,
+  AltRight = 104,
+  SuperLeft = 105,
+  SuperRight = 106,
+  CapsLock = 107,
+  NumLock = 108,
+  Help = 109,
+  Menu = 110,
+  Power = 111,
+  Sleep = 112,
+  Wake = 113,
+  Mute = 114,
+  VolumeDown = 115,
+  VolumeUp = 116,
+  MediaPlayPause = 117,
+  MediaStop = 118,
+  MediaNext = 119,
+  MediaPrevious = 120,
+  BrightnessDown = 121,
+  BrightnessUp = 122,
+  Eject = 123,
+  CharacterInput = 0xFFFE,
+  BackspaceDelete = 0xFFFF,
 };
 
 inline std::string keyToString(Key key) {
   switch (key) {
+  case Key::A:
+    return "A";
+  case Key::B:
+    return "B";
+  case Key::C:
+    return "C";
+  case Key::D:
+    return "D";
+  case Key::E:
+    return "E";
+  case Key::F:
+    return "F";
+  case Key::G:
+    return "G";
+  case Key::H:
+    return "H";
+  case Key::I:
+    return "I";
+  case Key::J:
+    return "J";
+  case Key::K:
+    return "K";
+  case Key::L:
+    return "L";
+  case Key::M:
+    return "M";
+  case Key::N:
+    return "N";
+  case Key::O:
+    return "O";
+  case Key::P:
+    return "P";
+  case Key::Q:
+    return "Q";
+  case Key::R:
+    return "R";
+  case Key::S:
+    return "S";
+  case Key::T:
+    return "T";
+  case Key::U:
+    return "U";
+  case Key::V:
+    return "V";
+  case Key::W:
+    return "W";
+  case Key::X:
+    return "X";
+  case Key::Y:
+    return "Y";
+  case Key::Z:
+    return "Z";
   case Key::Num0:
-    return "Num0";
+    return "0";
   case Key::Num1:
-    return "Num1";
+    return "1";
   case Key::Num2:
-    return "Num2";
+    return "2";
   case Key::Num3:
-    return "Num3";
+    return "3";
   case Key::Num4:
-    return "Num4";
+    return "4";
   case Key::Num5:
-    return "Num5";
+    return "5";
   case Key::Num6:
-    return "Num6";
+    return "6";
   case Key::Num7:
-    return "Num7";
+    return "7";
   case Key::Num8:
-    return "Num8";
+    return "8";
   case Key::Num9:
-    return "Num9";
-  case Key::Enter:
-    return "Enter";
-  case Key::Escape:
-    return "Escape";
-  case Key::Backspace:
-    return "Backspace";
-  case Key::Tab:
-    return "Tab";
-  case Key::Space:
-    return "Space";
-  case Key::Left:
-    return "Left";
-  case Key::Right:
-    return "Right";
-  case Key::Up:
-    return "Up";
-  case Key::Down:
-    return "Down";
+    return "9";
   case Key::F1:
     return "F1";
   case Key::F2:
@@ -130,10 +225,149 @@ inline std::string keyToString(Key key) {
     return "F11";
   case Key::F12:
     return "F12";
+  case Key::F13:
+    return "F13";
+  case Key::F14:
+    return "F14";
+  case Key::F15:
+    return "F15";
+  case Key::F16:
+    return "F16";
+  case Key::F17:
+    return "F17";
+  case Key::F18:
+    return "F18";
+  case Key::F19:
+    return "F19";
+  case Key::F20:
+    return "F20";
+  case Key::Enter:
+    return "Enter";
+  case Key::Escape:
+    return "Escape";
+  case Key::Backspace:
+    return "Backspace";
+  case Key::Tab:
+    return "Tab";
+  case Key::Space:
+    return "Space";
+  case Key::Left:
+    return "Left";
+  case Key::Right:
+    return "Right";
+  case Key::Up:
+    return "Up";
+  case Key::Down:
+    return "Down";
+  case Key::Home:
+    return "Home";
+  case Key::End:
+    return "End";
+  case Key::PageUp:
+    return "PageUp";
+  case Key::PageDown:
+    return "PageDown";
+  case Key::Delete:
+    return "Delete";
+  case Key::Insert:
+    return "Insert";
+  case Key::PrintScreen:
+    return "PrintScreen";
+  case Key::ScrollLock:
+    return "ScrollLock";
+  case Key::Pause:
+    return "Pause";
+  case Key::NumpadDivide:
+    return "NumpadDivide";
+  case Key::NumpadMultiply:
+    return "NumpadMultiply";
+  case Key::NumpadMinus:
+    return "NumpadMinus";
+  case Key::NumpadPlus:
+    return "NumpadPlus";
+  case Key::NumpadEnter:
+    return "NumpadEnter";
+  case Key::NumpadDecimal:
+    return "NumpadDecimal";
+  case Key::Numpad0:
+    return "Numpad0";
+  case Key::Numpad1:
+    return "Numpad1";
+  case Key::Numpad2:
+    return "Numpad2";
+  case Key::Numpad3:
+    return "Numpad3";
+  case Key::Numpad4:
+    return "Numpad4";
+  case Key::Numpad5:
+    return "Numpad5";
+  case Key::Numpad6:
+    return "Numpad6";
+  case Key::Numpad7:
+    return "Numpad7";
+  case Key::Numpad8:
+    return "Numpad8";
+  case Key::Numpad9:
+    return "Numpad9";
+  case Key::ShiftLeft:
+    return "ShiftLeft";
+  case Key::ShiftRight:
+    return "ShiftRight";
+  case Key::CtrlLeft:
+    return "CtrlLeft";
+  case Key::CtrlRight:
+    return "CtrlRight";
+  case Key::AltLeft:
+    return "AltLeft";
+  case Key::AltRight:
+    return "AltRight";
+  case Key::SuperLeft:
+    return "SuperLeft";
+  case Key::SuperRight:
+    return "SuperRight";
+  case Key::CapsLock:
+    return "CapsLock";
+  case Key::NumLock:
+    return "NumLock";
+  case Key::Help:
+    return "Help";
+  case Key::Menu:
+    return "Menu";
+  case Key::Power:
+    return "Power";
+  case Key::Sleep:
+    return "Sleep";
+  case Key::Wake:
+    return "Wake";
+  case Key::Mute:
+    return "Mute";
+  case Key::VolumeDown:
+    return "VolumeDown";
+  case Key::VolumeUp:
+    return "VolumeUp";
+  case Key::MediaPlayPause:
+    return "MediaPlayPause";
+  case Key::MediaStop:
+    return "MediaStop";
+  case Key::MediaNext:
+    return "MediaNext";
+  case Key::MediaPrevious:
+    return "MediaPrevious";
+  case Key::BrightnessDown:
+    return "BrightnessDown";
+  case Key::BrightnessUp:
+    return "BrightnessUp";
+  case Key::Eject:
+    return "Eject";
+  case Key::CharacterInput:
+    return "CharacterInput";
+  case Key::BackspaceDelete:
+    return "BackspaceDelete";
+  case Key::Unknown:
   default:
     return "Unknown";
   }
-};
+}
 
 enum Mod : uint8_t {
   Mod_None = 0,
@@ -160,6 +394,7 @@ inline bool operator&(Mod left, Mod right) {
 struct KeyStroke {
   Key key{Key::Unknown};
   Mod mods{Mod_None};
+  std::optional<std::u32string> character; // For Unicode character input
 };
 
 struct Capabilities {
@@ -192,16 +427,25 @@ public:
   // Returns true if permissions are granted
   bool requestPermissions();
 
-  // Key press/release
+  // Key press/release with optional character data
+  // If keystroke.character is set, it will inject that Unicode character
+  bool keyDown(const KeyStroke &keystroke);
+  bool keyUp(const KeyStroke &keystroke);
+
+  // Convenience overloads for basic key input
   bool keyDown(Key key, Mod mods = Mod_None);
   bool keyUp(Key key, Mod mods = Mod_None);
 
   // Combined key press + release
+  bool tap(const KeyStroke &keystroke);
   bool tap(Key key, Mod mods = Mod_None);
 
-  // Type arbitrary Unicode text
+  // Type arbitrary Unicode text (any character in any language)
   bool typeText(const std::u32string &text);
   bool typeText(const std::string &utf8Text);
+
+  // Type a single Unicode character
+  bool typeCharacter(char32_t codepoint);
 
 private:
   // Platform-specific implementation (pimpl idiom)
--- src/input/input_macos.mm
+++ src/input/input_macos.mm
@@ -18,6 +18,7 @@ namespace {
 // Convert input::Key to macOS virtual key code (CGKeyCode)
 CGKeyCode keyToVirtualKeyCode(Key key) {
   switch (key) {
+  // Letters
   case Key::A:
     return kVK_ANSI_A;
   case Key::B:
@@ -70,6 +71,7 @@ CGKeyCode keyToVirtualKeyCode(Key key) {
     return kVK_ANSI_Y;
   case Key::Z:
     return kVK_ANSI_Z;
+  // Numbers
   case Key::Num0:
     return kVK_ANSI_0;
   case Key::Num1:
@@ -90,6 +92,40 @@ CGKeyCode keyToVirtualKeyCode(Key key) {
     return kVK_ANSI_8;
   case Key::Num9:
     return kVK_ANSI_9;
+  // Numpad
+  case Key::Numpad0:
+    return kVK_ANSI_Keypad0;
+  case Key::Numpad1:
+    return kVK_ANSI_Keypad1;
+  case Key::Numpad2:
+    return kVK_ANSI_Keypad2;
+  case Key::Numpad3:
+    return kVK_ANSI_Keypad3;
+  case Key::Numpad4:
+    return kVK_ANSI_Keypad4;
+  case Key::Numpad5:
+    return kVK_ANSI_Keypad5;
+  case Key::Numpad6:
+    return kVK_ANSI_Keypad6;
+  case Key::Numpad7:
+    return kVK_ANSI_Keypad7;
+  case Key::Numpad8:
+    return kVK_ANSI_Keypad8;
+  case Key::Numpad9:
+    return kVK_ANSI_Keypad9;
+  case Key::NumpadDivide:
+    return kVK_ANSI_KeypadDivide;
+  case Key::NumpadMultiply:
+    return kVK_ANSI_KeypadMultiply;
+  case Key::NumpadMinus:
+    return kVK_ANSI_KeypadMinus;
+  case Key::NumpadPlus:
+    return kVK_ANSI_KeypadPlus;
+  case Key::NumpadEnter:
+    return kVK_ANSI_KeypadEnter;
+  case Key::NumpadDecimal:
+    return kVK_ANSI_KeypadDecimal;
+  // Control keys
   case Key::Enter:
     return kVK_Return;
   case Key::Escape:
@@ -100,6 +136,11 @@ CGKeyCode keyToVirtualKeyCode(Key key) {
     return kVK_Tab;
   case Key::Space:
     return kVK_Space;
+  case Key::Delete:
+    return kVK_ForwardDelete;
+  case Key::Insert:
+    return kVK_Help;
+  // Navigation
   case Key::Left:
     return kVK_LeftArrow;
   case Key::Right:
@@ -108,6 +149,15 @@ CGKeyCode keyToVirtualKeyCode(Key key) {
     return kVK_UpArrow;
   case Key::Down:
     return kVK_DownArrow;
+  case Key::Home:
+    return kVK_Home;
+  case Key::End:
+    return kVK_End;
+  case Key::PageUp:
+    return kVK_PageUp;
+  case Key::PageDown:
+    return kVK_PageDown;
+  // Function keys
   case Key::F1:
     return kVK_F1;
   case Key::F2:
@@ -132,9 +182,72 @@ CGKeyCode keyToVirtualKeyCode(Key key) {
     return kVK_F11;
   case Key::F12:
     return kVK_F12;
-  case Key::Unknown:
-  default:
-    return UINT16_MAX; // Invalid key code
+  case Key::F13:
+    return kVK_F13;
+  case Key::F14:
+    return kVK_F14;
+  case Key::F15:
+    return kVK_F15;
+  case Key::F16:
+    return kVK_F16;
+  case Key::F17:
+    return kVK_F17;
+  case Key::F18:
+    return kVK_F18;
+  case Key::F19:
+    return kVK_F19;
+  case Key::F20:
+    return kVK_F20;
+  // Modifiers as keys
+  case Key::ShiftLeft:
+    return kVK_Shift;
+  case Key::ShiftRight:
+    return kVK_RightShift;
+  case Key::CtrlLeft:
+    return kVK_Control;
+  case Key::CtrlRight:
+    return kVK_RightControl;
+  case Key::AltLeft:
+    return kVK_Option;
+  case Key::AltRight:
+    return kVK_RightOption;
+  case Key::SuperLeft:
+    return kVK_Command;
+  case Key::SuperRight:
+    return kVK_RightCommand;
+  // Lock keys
+  case Key::CapsLock:
+   return kVK_CapsLock;
+ case Key::NumLock:
+   return kVK_ANSI_KeypadClear;
+ // Special keys
+ case Key::PrintScreen:
+   return kVK_F13;
+ case Key::ScrollLock:
+   return kVK_ANSI_KeypadClear;
+ case Key::Pause:
+   return kVK_F15;
+ case Key::Menu:
+   return kVK_F4;
+ case Key::Mute:
+   return kVK_Mute;
+ case Key::VolumeDown:
+   return kVK_VolumeDown;
+ case Key::VolumeUp:
+   return kVK_VolumeUp;
+ case Key::MediaPlayPause:
+   return kVK_F8;
+ case Key::MediaNext:
+   return kVK_F9;
+ case Key::MediaPrevious:
+   return kVK_F7;
+ case Key::MediaStop:
+   return kVK_F8;
+ case Key::CharacterInput:
+ case Key::BackspaceDelete:
+ case Key::Unknown:
+ default:
+   return UINT16_MAX;
   }
 }
 
@@ -278,7 +391,34 @@ struct InputBackend::Impl {
     return postKeyEvent(keyCode, false, modsToCGFlags(mods));
   }
 
-  bool tap(Key key, Mod mods) { return keyDown(key, mods) && keyUp(key, mods); }
+  bool tap(Key key, Mod mods) {
+    return keyDown(key, mods) && keyUp(key, mods);
+  }
+
+  // Handle KeyStroke with optional character data
+  bool keyDown(const KeyStroke &keystroke) {
+    if (keystroke.character && keystroke.character->size() > 0) {
+      // Type the character(s) instead of pressing a key
+      return typeText(*keystroke.character);
+    }
+    return keyDown(keystroke.key, keystroke.mods);
+  }
+
+  bool keyUp(const KeyStroke &keystroke) {
+    if (keystroke.character && keystroke.character->size() > 0) {
+      // For character input, we don't have a keyUp
+      return true;
+    }
+    return keyUp(keystroke.key, keystroke.mods);
+  }
+
+  bool tap(const KeyStroke &keystroke) {
+    if (keystroke.character && keystroke.character->size() > 0) {
+      // For character input, just type it
+      return typeText(*keystroke.character);
+    }
+    return tap(keystroke.key, keystroke.mods);
+  }
 
   bool typeText(const std::u32string &text) {
     if (text.empty()) {
@@ -324,6 +464,11 @@ struct InputBackend::Impl {
   bool typeText(const std::string &utf8Text) {
     return typeText(utf8ToUtf32(utf8Text));
   }
+
+  bool typeCharacter(char32_t codepoint) {
+    std::u32string text(1, codepoint);
+    return typeText(text);
+  }
 };
 
 // InputBackend public interface implementation
@@ -356,12 +501,24 @@ bool InputBackend::requestPermissions() {
   return m_impl && m_impl->requestPermissions();
 }
 
+bool InputBackend::keyDown(const KeyStroke &keystroke) {
+  return m_impl && m_impl->keyDown(keystroke);
+}
+
+bool InputBackend::keyUp(const KeyStroke &keystroke) {
+  return m_impl && m_impl->keyUp(keystroke);
+}
+
 bool InputBackend::keyDown(Key key, Mod mods) {
   return m_impl && m_impl->keyDown(key, mods);
 }
 
 bool InputBackend::keyUp(Key key, Mod mods) {
   return m_impl && m_impl->keyUp(key, mods);
+}
+
+bool InputBackend::tap(const KeyStroke &keystroke) {
+  return m_impl && m_impl->tap(keystroke);
 }
 
 bool InputBackend::tap(Key key, Mod mods) {
@@ -374,6 +531,10 @@ bool InputBackend::typeText(const std::u32string &text) {
 
 bool InputBackend::typeText(const std::string &utf8Text) {
   return m_impl && m_impl->typeText(utf8Text);
+}
+
+bool InputBackend::typeCharacter(char32_t codepoint) {
+  return m_impl && m_impl->typeCharacter(codepoint);
 }
 
 } // namespace input
